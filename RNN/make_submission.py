01e57e8e3e54ecf6fd0d77b1026117ea4c4f989a
yoonseok312 <yoonseok@berkeley.edu>
2021-07-02 04:38:49 +0900

    :art: add make submission
    add make submission

--- RNN/make_submission.py
+++ RNN/make_submission.py
@@ -21,62 +21,56 @@ def make_submission():
     # output_dim = 2  # output dimension
     # visible_gpus = 0
     # seq_len = 20
-    batch_size = 16
-    n_iters = 40000
+
+    # LSTM configs
+    batch_size = 50
+    n_iters = 50000
     visible_gpus = 0
-    seed = 777
+    seed = 7777
+
     # Create RNN
     input_dim = 128  # input dimension
-    hidden_dim = 256  # hidden layer dimension
-    layer_dim = 4  # number of hidden layers
+    hidden_dim = 256 # hidden layer dimension
+    layer_dim = 1  # number of hidden layers
     output_dim = 2  # output dimension
-    seq_len = 40
+    seq_len = 20
 
-    #LSTM
-    # batch_size = 32
-    # n_iters = 20000
-    # visible_gpus = 0
-    # seed = 777
-    # # Create RNN
-    # input_dim = 128  # input dimension
-    # hidden_dim = 256  # hidden layer dimension
-    # layer_dim = 1  # number of hidden layers
-    # output_dim = 2  # output dimension
-    # seq_len = 20
 
-    device = "cpu" if visible_gpus == '-1' else f"cuda:{visible_gpus}"
+
+    device = "cpu" if visible_gpus == '-1' else f"cuda"
     device_id = 0 if device == f"cuda" else -1
 
-    model = RNNModel(input_dim, hidden_dim, layer_dim, output_dim, device)
-    # model = LSTMModel(input_dim, hidden_dim, layer_dim, output_dim, device).to(device=device)
-    model.load_state_dict((torch.load('./model/rnn_seq_len_40/model_15000.pth')))
+    # model = RNNModel(input_dim, hidden_dim, layer_dim, output_dim, device)
+    model = LSTMModel(input_dim, hidden_dim, layer_dim, output_dim, device).to(device=device)
+    model.load_state_dict((torch.load('./model/article_rnn/model_91000.pth')))
 
-    # train_data = pd.read_pickle(f"./data/train_df.pickle")
-    tokenized_data, tokenized_valid_data, embedding_model,  train_data, valid_data, l_tokenizer = tokenize(input_dim)
+    train_data = pd.read_pickle(f"./data/train_df.pickle")
+    valid_data = pd.read_pickle(f"./data/valid_df.pickle")
+    test_data = pd.read_pickle(f"./data/test_df.pickle")
+
+    train_sent = train_data['sentence']
+    valid_sent = valid_data['sentence']
+    test_sent = test_data['sentence']
+    all_sent = pd.concat([train_sent, valid_sent, test_sent])
+
+    tokenized_data, tokenized_valid_data, embedding_model, target_train, target_test, l_tokenizer = tokenize(input_dim)
 
     word_extractor = WordExtractor()
-    word_extractor.train(train_data['sentence'])
+    word_extractor.train(all_sent)
     # word_score_table = word_extractor.extract()
 
     with open("./data/test.json", "r", encoding='UTF-8-sig') as st_json:
         test = json.load(st_json)
-
     test_sent_list = []
-
     for article in test:
         original = article['article_original']
         test_sent_list += original
-
     test_data = pd.DataFrame(test_sent_list, columns=['sentence'])
-
     for c in ",.:;":
         test_data["sentence"] = test_data["sentence"].str.replace(c, "")
-
     for c in "()[]":
         test_data["sentence"] = test_data["sentence"].str.replace(c, " ")
-
     tokenized_test_data = [l_tokenizer.tokenize(sentence, flatten=True) for sentence in test_data['sentence']]
-
     test_list = []
     zero_list = [0] * input_dim
     for sent in tokenized_test_data:
@@ -101,28 +95,29 @@ def make_submission():
 
     output_list = []
     count = 0
-    for i, (images, labels) in enumerate(test_loader):
-        for_test = Variable(images.view(-1, seq_len, input_dim))
-
-        if torch.cuda.is_available():
-            for_test.to(device=f"cuda:{visible_gpus}")
-
-        # Forward propagation
-        output = model(for_test)
-        output = torch.nn.functional.softmax(output, dim=1)
-        output_list.append(output.data[0][1])
+    with torch.no_grad():
+        model.eval()
+        for i, (images, labels) in enumerate(test_loader):
+            for_test = images.view(-1, seq_len, input_dim)
+            if torch.cuda.is_available():
+                for_test.to(device=f"cuda")
+            # Forward propagation
+            # h0_ = torch.zeros(layer_dim, for_test.size(0), hidden_dim).to(device="cuda")
+            # c0_ = torch.zeros(layer_dim, for_test.size(0), hidden_dim).to(device="cuda")
+            output = model(for_test)
+            output = torch.nn.functional.sigmoid(output)
+            output_list.append(output[0][1].item())
 
     test_data["if_ext"] = output_list
 
     sub = []
     sent_count = 0
-
     for n, data in enumerate(test):
         article_original = data['article_original']
         id = data['id']
         result = []
         for sent_num, sent in enumerate(article_original):
-            ext_pos = test_data.iloc[sent_count, 1].item()
+            ext_pos = test_data.iloc[sent_count, 1]
             if len(result) >= 3:
                 result = sorted(result, key=(lambda x: x[1]), reverse=True)
                 if ext_pos > result[-1][1]:
@@ -135,15 +130,13 @@ def make_submission():
             print(result)
         sorted(result, key=(lambda x: x[1]), reverse=True)
         sub.append(result)
-
     with open("./output/sample_submission.json", "r") as st_json:
         submission_template = json.load(st_json)
-
     for n in range(len(sub)):
         for i in range(3):
             submission_template[n]['summary_index' + str(i + 1)] = sub[n][i][0]
 
-    with open("./output/submission_rnn_40.json", "w") as json_file:
+    with open("./output/article_final.json", "w") as json_file:
         json.dump(submission_template, json_file)
 
 if __name__ == '__main__':
